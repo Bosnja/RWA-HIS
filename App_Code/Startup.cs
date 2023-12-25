75afddcc7b64643e703abfa3b006efa490ca92ae
Deniz Hadžirušidović <deniz-20114@outlook.com>
2023-12-25 15:29:52 +0100

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RWA_HIS.Startup))]
+namespace RWA_HIS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
